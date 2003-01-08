27377eadabcfa9731eeced0bf3e0a715a2c91ee7
Jordi Albornoz <jordi@src.gnome.org>
2003-01-08 20:23:01 +0000

    Bah, still broken, but less. Gettextize is STOOPEED.

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,21 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gtetrinet"
+PKG_NAME="gnome-mud"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
+
+if [ -f configure.in ]; then
+    echo "configure.in is not a symlink to configure.ac."
+    echo "Move it out of the way and try again."
+elif [ ! -L configure.in ]; then
+    rm -f configure.in
+    ln -s configure.ac configure.in
+fi
 
 which gnome-autogen.sh || {
 	echo "You need to install gnome-common from the GNOME CVS"
@@ -18,3 +26,5 @@ which gnome-autogen.sh || {
 }
 
 USE_GNOME2_MACROS=1 . gnome-autogen.sh
+
+rm -f configure.in
