f529ea30d6bdd7f221114e274da88a5f1fecbbef
Jordi Albornoz <jordi@src.gnome.org>
2003-01-08 20:08:26 +0000

    Remove m4 stuff. Unneeded for GNOME2. Switch to gnome-autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,47 +1,20 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-# This was lifted from the Gimp, and adapted slightly by
-# Raph Levien and Robin Ericsson.
 
-DIE=0
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-PROG="GNOME-Mud"
-        
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-        echo 
-        echo "You must have autoconf installed to compile $PROG."
-        echo "Download the appropriate package for your distribution,"
-        echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
+PKG_NAME="gtetrinet"
+
+(test -f $srcdir/configure.in) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile $PROG."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-        echo "(or a newer version if it is available)"
-        DIE=1
+which gnome-autogen.sh || {
+	echo "You need to install gnome-common from the GNOME CVS"
+	exit 1
 }
-if test "$DIE" -eq 1; then
-        exit 1
-fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-        
-for dir in .   
-do
-  echo processing $dir
-  (cd $dir; \
-  aclocalinclude="$ACLOCAL_FLAGS" && \
-#  aclocal -I macros $aclocalinclude && \
-  aclocal && \
-  autoheader &&  automake --add-missing &&
-  autoconf)
-done
-
-if test x$1 != xno ; then
-  ./configure "$@"
-fi
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
