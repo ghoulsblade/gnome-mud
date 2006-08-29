851d17cd88a08b344db49833272bfac5fc477383
Mart Raudsepp <leio@dustbite.net>
2006-08-29 20:07:41 +0000

    Bump GTK_REQUIRED to 2.10.0 for GtkStatusIcon. Port to GtkStatusIcon to
    2006-08-29  Mart Raudsepp  <leio@dustbite.net>
    
    	* configure.ac: Bump GTK_REQUIRED to 2.10.0 for GtkStatusIcon.
    	* src/mud-tray.c: Port to GtkStatusIcon to get rid of libegg.
    	Fixes #69909.
    	* src/Makefile.am (gnome_mud_SOURCES): Remove eggtrayicon.[ch].
    	* src/eggtrayicon.[ch]: Removed.

--- src/mud-tray.c
+++ src/mud-tray.c
@@ -25,7 +25,6 @@
 #include <gtk/gtkwidget.h>
 
 #include "gnome-mud.h"
-#include "eggtrayicon.h"
 #include "mud-connection-view.h"
 #include "mud-window.h"
 #include "mud-tray.h"
@@ -33,13 +32,12 @@
 struct _MudTrayPrivate
 {
 	MudWindow *mainWindow;
-	
+
 	GtkWidget *window;
-	EggTrayIcon *tray_icon;
-	GtkWidget *image;
+	GtkStatusIcon *icon;
 	gboolean window_invisible;
 };
-	
+
 GType mud_tray_get_type (void);
 static void mud_tray_init (MudTray *tray);
 static void mud_tray_class_init (MudTrayClass *klass);
@@ -48,10 +46,7 @@ static void mud_tray_finalize (GObject *object);
 void mud_tray_window_toggle(GtkWidget *widget, MudTray *tray);
 void mud_tray_window_exit(GtkWidget *widget, MudTray *tray);
 gboolean mud_tray_create_cb(gpointer data);
-void mud_tray_destroyed_cb(GtkWidget *widget, void *data);
-void mud_tray_clicked_cb(GtkWidget *button, GdkEventButton *event, void *data);
 
-void mud_tray_menu(MudTray *tray);
 void mud_tray_create(MudTray *tray);
 void mud_tray_destroy(MudTray *tray);
 
@@ -91,10 +86,9 @@ mud_tray_init (MudTray *tray)
 	tray->priv = g_new0(MudTrayPrivate, 1);
 
 	tray->priv->window = NULL;
-	tray->priv->tray_icon = NULL;
-	tray->priv->image = NULL;
+	tray->priv->icon = NULL;
 	tray->priv->window_invisible = FALSE;
-	
+
         mud_tray_create(tray);
 }
 
@@ -114,7 +108,7 @@ mud_tray_finalize (GObject *object)
 
 	tray = MUD_TRAY(object);
 
-	mud_tray_destroy(tray);        
+	mud_tray_destroy(tray);
 	g_free(tray->priv);
 
 	parent_class = g_type_class_peek_parent(G_OBJECT_GET_CLASS(object));
@@ -139,50 +133,20 @@ void mud_tray_window_exit(GtkWidget *widget, MudTray *tray)
 gboolean mud_tray_create_cb(gpointer data)
 {
 	MudTray *tray = MUD_TRAY(data);
-	
+
         mud_tray_create(tray);
 
         return FALSE; /* for when we're called by the glib idle handler */
 }
 
-
-void mud_tray_destroyed_cb(GtkWidget *widget, void *data)
+void mud_tray_activate_cb(GtkStatusIcon *icon, MudTray *tray)
 {
-	MudTray *tray = MUD_TRAY(data);
-	
-        g_object_unref(G_OBJECT(tray->priv->tray_icon));
-        tray->priv->tray_icon = NULL;
-
-        // for some reason, our tray icon was destroyed, re-create it!
-        g_idle_add(mud_tray_create_cb, &data);
-}
-
-void mud_tray_clicked_cb(GtkWidget *button, GdkEventButton *event, void *data)
-{
-	MudTray *tray = MUD_TRAY(data);
-	
-        if (event->type != GDK_BUTTON_PRESS)
-                return;
-
-        switch (event->button) {
-                case 1:
-                        // left-mouse button
-                        mud_tray_window_toggle(NULL, tray);
-                        break;
-                case 2:
-                        // middle-mouse button
-                        // suggestions anyone?
-                        break;
-                case 3:
-                        mud_tray_menu(tray);
-                        break;
-        }
+	mud_tray_window_toggle(NULL, tray);
 }
 
 
-
-// functions
-void mud_tray_menu(MudTray *tray)
+void mud_tray_popup_menu_cb(GtkStatusIcon *icon, guint button,
+                            guint activate_time, MudTray *tray)
 {
         static GtkWidget *menu = NULL;
         GtkWidget *entry;
@@ -206,7 +170,10 @@ void mud_tray_menu(MudTray *tray)
         g_signal_connect(G_OBJECT(entry), "activate", G_CALLBACK(mud_tray_window_exit), tray);
 
         gtk_widget_show_all(menu);
-        gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, 0, gtk_get_current_event_time());
+        gtk_menu_popup(GTK_MENU(menu),
+                       NULL, NULL,
+                       gtk_status_icon_position_menu, icon,
+                       button, activate_time);
 }
 
 void mud_tray_update_icon(MudTray *tray, enum mud_tray_status icon)
@@ -226,48 +193,34 @@ void mud_tray_update_icon(MudTray *tray, enum mud_tray_status icon)
                         break;
         }
 
-        gtk_image_set_from_file(GTK_IMAGE(tray->priv->image), icon_name);
+        gtk_status_icon_set_from_file(tray->priv->icon, icon_name);
 }
 
-void 
+void
 mud_tray_create(MudTray *tray)
 {
-	GtkWidget *box;
-
-        if (tray->priv->tray_icon) {
+        if (tray->priv->icon) {
                 /* if this is being called when a tray icon exists, it's because
                    something messed up. try destroying it before we proceed */
                 g_message("Trying to create icon but it already exists?\n");
                 mud_tray_destroy(tray);
         }
-        
-	tray->priv->tray_icon = egg_tray_icon_new(_("GNOME Mud"));
-        box = gtk_event_box_new();
-        tray->priv->image = gtk_image_new();
 
-        g_signal_connect(G_OBJECT(tray->priv->tray_icon), "destroy", G_CALLBACK(mud_tray_destroyed_cb), tray);
-        g_signal_connect(G_OBJECT(box), "button-press-event", G_CALLBACK(mud_tray_clicked_cb), tray);
-
-        gtk_container_add(GTK_CONTAINER(box), tray->priv->image);
-        gtk_container_add(GTK_CONTAINER(tray->priv->tray_icon), box);
+        tray->priv->icon = gtk_status_icon_new(); /*(_("GNOME Mud"));*/
+        g_signal_connect(tray->priv->icon, "activate",
+                         G_CALLBACK (mud_tray_activate_cb), tray);
+        g_signal_connect(tray->priv->icon, "popup_menu",
+                         G_CALLBACK (mud_tray_popup_menu_cb), tray);
 
         mud_tray_update_icon(tray, offline_connecting);
-
-        gtk_widget_show_all(GTK_WIDGET(tray->priv->tray_icon));
-
-        /* ref the tray icon before we bandy it about the place */
-        g_object_ref(G_OBJECT(tray->priv->tray_icon));
 }
 
 void
 mud_tray_destroy(MudTray *tray)
 {
-        g_signal_handlers_disconnect_by_func(G_OBJECT(tray->priv->tray_icon), G_CALLBACK(mud_tray_destroyed_cb), tray);
-        gtk_widget_destroy(GTK_WIDGET(tray->priv->tray_icon));
+        g_object_unref(G_OBJECT(tray->priv->icon));
+        tray->priv->icon = NULL;
 
-        g_object_unref(G_OBJECT(tray->priv->tray_icon));
-        tray->priv->tray_icon = NULL;
-        
         g_object_unref(tray->priv->mainWindow);
 }
 
@@ -282,5 +235,5 @@ mud_tray_new(MudWindow *mainWindow, GtkWidget *window)
 	tray->priv->window = window;
 	tray->priv->mainWindow = mainWindow;
 
-	return tray;	
+	return tray;
 }
