c742b25e04896b8d522b3cf32d47e9db7610836a
Les Harris <lharris@src.gnome.org>
2008-06-20 16:19:14 +0000

    Les Harris: Fixed a segmetation fault that occured when history was used with no active connection.
    svn path=/trunk/; revision=664

--- src/mud-window.c
+++ src/mud-window.c
@@ -279,29 +279,32 @@ mud_window_textview_keypress(GtkWidget *widget, GdkEventKey *event, MudWindow *w
 
 		return TRUE;
 	}
+
+	if(window->priv->current_view)
+	{	
+		if(event->keyval == GDK_Up)
+		{
+		    text = mud_connection_view_get_history_item(
+		        MUD_CONNECTION_VIEW(window->priv->current_view), HISTORY_UP);
+	    
+	   		gtk_text_buffer_set_text(buffer, text, strlen(text));
+	   	 	gtk_text_buffer_get_bounds(buffer, &start, &end);
+	   	 	gtk_text_buffer_select_range(buffer, &start, &end);
+	    
+	    	return TRUE;
+		}
 	
-	if(event->keyval == GDK_Up)
-	{
-	    text = mud_connection_view_get_history_item(
-	        MUD_CONNECTION_VIEW(window->priv->current_view), HISTORY_UP);
+		if(event->keyval == GDK_Down)
+		{
+		    text = mud_connection_view_get_history_item(
+		        MUD_CONNECTION_VIEW(window->priv->current_view), HISTORY_DOWN);
 	    
-	    gtk_text_buffer_set_text(buffer, text, strlen(text));
-	    gtk_text_buffer_get_bounds(buffer, &start, &end);
-	    gtk_text_buffer_select_range(buffer, &start, &end);
+		    gtk_text_buffer_set_text(buffer, text, strlen(text));
+		    gtk_text_buffer_get_bounds(buffer, &start, &end);
+		    gtk_text_buffer_select_range(buffer, &start, &end);
 	    
-	    return TRUE;
-	}
-	
-	if(event->keyval == GDK_Down)
-	{
-	    text = mud_connection_view_get_history_item(
-	        MUD_CONNECTION_VIEW(window->priv->current_view), HISTORY_DOWN);
-	    
-	    gtk_text_buffer_set_text(buffer, text, strlen(text));
-	    gtk_text_buffer_get_bounds(buffer, &start, &end);
-	    gtk_text_buffer_select_range(buffer, &start, &end);
-	    
-	    return TRUE;
+		    return TRUE;
+		}
 	}
 
 	return FALSE;
